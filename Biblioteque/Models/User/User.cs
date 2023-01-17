cc229f27803c02a9ce00483a2256c8cdd30d1f62
gregory <gregory.schoemaecker@outlook.fr>
2023-01-17 18:05:38 +0100

    Authentification

---
+++ Biblioteque/Models/User/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteque.Models.User
+{
+    public class User : AbstractEntity
+    {
+        public int LivreId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }        
+
+        // user ID from AspNetUser table.
+         
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; } 
+        
+        public string Password { get; set; }
+    }
+}
+
